16ca5d34687ac778c37a6e280ee03946e37a9630
Twinkiesss <138775469+Twinkiesss@users.noreply.github.com>
2023-12-17 15:28:31 +0300

    Update triangle.py
    wrong input

--- triangle.py
+++ triangle.py
@@ -1,7 +1,11 @@
 def area(a, h):
+    if a <= 0 or h <= 0:
+        raise ValueError('Wrong input')
     '''Принимает значение a, h, возвращает площадь треугольника со стороной а и проведенной к ней высотой h. Example: a = 3, b = 4 => S = 6'''
     return a * h / 2
 
 def perimeter(a, b, c):
+    if a <= 0 or b <= 0 or c <= 0 or a>= b+c or b >= a+c or c>= a+b:
+        raise ValueError('Wrong input')
     '''Принимает значения a, b, c, возвращает их сумму. Example: a = 4, b = 2, c = 3 => P = 9'''
     return a + b + c 
