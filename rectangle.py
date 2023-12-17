aa26d4f1303f57cf990731d547cf881cdc0400c6
Twinkiesss <138775469+Twinkiesss@users.noreply.github.com>
2023-12-17 15:15:56 +0300

    Update rectangle.py
    wrong input

--- rectangle.py
+++ rectangle.py
@@ -1,8 +1,12 @@
 def area(a, b):
+    if (a <= 0 or b<= 0):
+        raise ValueError('Wrong input')
     '''Принимает числа a, b, возвращает их произведение. Example: first side = 1, second side = 2 => S = 2'''
     return a * b 
 
 def perimeter(a, b):
+    if (a <= 0 or b<=0):
+        raise ValueError('Wrong input')
     '''Принимает числа a, b, возвращает их удвоенную сумму. Example: first side = 1, second side = 2 => P = 6'''
     return a + b + a + b
 
