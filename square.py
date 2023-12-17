3d7997a3f311fd8ae1d35d2752897afbb9e1481c
Twinkiesss <138775469+Twinkiesss@users.noreply.github.com>
2023-12-17 15:14:41 +0300

    Update square.py
    wrong input

--- square.py
+++ square.py
@@ -1,9 +1,13 @@
 
 def area(a):
+    if (a <= 0):
+        raise ValueError('Wrong input')
     '''Принимает значение a, возвращает его квадрат. Example: a = 2 => S = 4'''
     return a * a
 
 
 def perimeter(a):
+    if (a <= 0):
+        raise ValueError('Wrong input')
     '''Принимает значение а, возвращает периметр квадрата со стороной а. Example: a = 2 => P = 8'''
     return 4 * a
