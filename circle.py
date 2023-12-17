9d77e03eb01b9b8b0573ea32fb53e8a5273938cc
Twinkiesss <138775469+Twinkiesss@users.noreply.github.com>
2023-12-17 15:17:48 +0300

    Update circle.py
    wrong input

--- circle.py
+++ circle.py
@@ -2,11 +2,15 @@ import math
 
 
 def area(r):
+    if r <= 0:
+        raise ValueError('Wrong input')
     '''Принимает число r, возвращает площадь круга с радиусом r. Example: r = 2 => S = pi * 4 ~ 12,56'''
     return math.pi * r * r
 
 
 def perimeter(r):
+    if r <= 0:
+        raise ValueError('Wrong input')
     '''Принимает число r, возвращает периметр круга. Example: r = 2 => P = pi * 4 ~ 12,56'''
     return 2 * math.pi * r
 
